f3c0cbb5872ba1848982f578afdb4ccc9fd52d37
justdheja <justdheja@gmail.com>
2020-06-16 19:21:09 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created() {
+    fetch('https://covid19.mathdro.id/api/countries/indonesia')
+      .then(response => response.json())
+      .then(response => console.log(response))
+  },
+}
+</script>
