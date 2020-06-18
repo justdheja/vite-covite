8320f49dfa57fc50b768dba086aefcfd3dce1acc
justdheja <justdheja@gmail.com>
2020-06-18 09:39:16 +0700

    fix: styling on hero

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <section class="hero is-success is-fullheight">
+    <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container">
           <div class="columns">
-            <div class="column">
+            <div class="column text-place">
               <h1 class="title">
-                Fullheight title
+                new normal
               </h1>
               <h2 class="subtitle">
-                Fullheight subtitle
+                Brace yourself new normal is coming
               </h2>
             </div>
-            <div class="column">
-              helo
+            <div class="column bg-mask">
+              <img src="../assets/social_distancing.svg" alt="">
             </div>
           </div>
         </div>
@@ -21,3 +21,41 @@
     </section>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Merriweather&family=Pacifico&display=swap');
+
+$pacifico : 'Pacifico', cursive;
+$merriweather : 'Merriweather', serif;
+$bg-hero : #F5F4F4;
+
+.hero{
+  background: $bg-hero;
+}
+
+.text-place{
+  text-align: left;
+  justify-content: center;
+  margin: auto;
+}
+
+
+.title{
+  font-family: $pacifico;
+  color: black;
+}
+
+.subtitle{
+  font-family: $merriweather;
+  font-size: 3vh;
+}
+
+.bg-mask{
+  text-align: center;
+}
+
+img{
+  height: 50vh;
+}
+</style>
